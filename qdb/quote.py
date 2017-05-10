10a905c409520fb9918c3d16abe137eee653772a
Nolan Lum <nolanlum@users.noreply.github.com>
2017-05-10 01:49:12 -0700

    Add endpoint to get a random quote. (#3)

--- qdb/quote.py
+++ qdb/quote.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from flask import abort, g
 
 from qdb.auth import google_authenticate
@@ -37,6 +39,20 @@ def post(body):
 def find(query):
     quotes = Quote.objects.search_text(query).order_by('-id')
     return [quote for quote in quotes]
+
+
+@google_authenticate
+def rand():
+    max_num_quote = Quote.objects.order_by('-num').scalar('num').first()
+    if not max_num_quote:
+        abort(404)
+
+    rand_num = randint(1, max_num_quote)
+    rand_quote = Quote.objects(
+        num__lte=rand_num,
+    ).order_by('-num').first()
+
+    return rand_quote
 
 
 @google_authenticate
